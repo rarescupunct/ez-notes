fafa47b223505a7c2e193f1c63de01a6cc08dd8f
Fira-Slavu Rareș-Florin <raresdragutuu@gmail.com>
2026-04-17 21:49:38 +0300

    tried to fix it

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -1,19 +1,20 @@
-import{db} from "@/lib/db";
-import {NextResponse} from "next/server";
-import { notes } from "@/lib/schema";
-import { eq }  from "drizzle-orm";
+import { db } from '@/lib/db';
+import { notes } from '@/lib/schema';
+import { eq } from 'drizzle-orm';
+import { NextResponse } from 'next/server';
 
-export async function PATCH(req: Request, {params}:{params: {id: number}}) {
-    const { name, body } = await req.json();
-    const updated = await db.update(notes)
-                .set({ name, body, editedAt: new Date() })
-                .where(eq(notes.id, params.id))
-                .returning();
-    return NextResponse.json(updated[0]);
+export async function PATCH(req: Request, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const { name, body } = await req.json();
+  const updated = await db.update(notes)
+    .set({ name, body, editedAt: new Date() })
+    .where(eq(notes.id, Number(id)))
+    .returning();
+  return NextResponse.json(updated[0]);
 }
 
-
-export async function DELETE(_req: Request, {params}:{params: {id: number}}) {
-    await db.delete(notes).where(eq(notes.id, params.id));
-    return NextResponse.json({success : true});
+export async function DELETE(_req: Request, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  await db.delete(notes).where(eq(notes.id, Number(id)));
+  return NextResponse.json({ success: true });
 }
